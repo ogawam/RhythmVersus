e99ea1414ad29397d7991efd0cec6dc3e0e0b459
ogawam <gm.lrhv@gmail.com>
2013-11-30 17:10:43 +0900

    Initial Commit

---
+++ RhythmVersus/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  RhythmVersus
+//
+//  Created by 小川 穣 on 2013/11/30.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ RhythmVersus/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  RhythmVersus
+//
+//  Created by 小川 穣 on 2013/11/30.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
